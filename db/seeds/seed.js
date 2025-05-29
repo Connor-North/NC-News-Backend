ad1b30b2210fefcc5c92cb97811ea30864095529
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-05-29 17:03:55 +0100

    Skip currently unused tests to push

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,12 @@
-const db = require("../connection")
+const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db.query(`DROP TABLE IF EXISTS topics;`).then(() => {
+    db.query(`CREATE TABLE topics(
+    slug VARCHAR(40) PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    img_url VARCHAR(1000));
+    `);
+  });
 };
 module.exports = seed;
