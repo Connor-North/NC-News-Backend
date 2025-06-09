5c757658c8d5b9795a8b0098872599c7240b0e46
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-06-09 14:38:00 +0100

    Write API documention for POST /:article_id/comments and update fetchArticleById to make the argument more explicit

--- models/ncnews.models.js
+++ models/ncnews.models.js
@@ -34,13 +34,13 @@ const fetchUsers = () => {
   });
 };
 
-const fetchArticleById = (id) => {
-  if (isNaN(id)) {
+const fetchArticleById = (article_id) => {
+  if (isNaN(article_id)) {
     return Promise.reject({ status: 400, msg: "Input must be a number" });
   }
 
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [id])
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "404 Not Found" });
